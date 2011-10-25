66633b0054b198a89fbf58d704fb8b69cd99f3db
Luca Spiller <luca@stackednotion.com>
2011-10-25 17:54:46 +0100

    Enable parsing history

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
 require 'httparty'
 require 'nokogiri'
@@ -42,6 +43,43 @@ class CycleHireApi
     true
   end
 
+  def fetch_history
+    options = {
+      :headers => {
+        'Cookie' => @cookies
+      }
+    }
+    response = self.class.get('/account/activity', options)
+
+    # get table
+    doc = Nokogiri::HTML(response.body)
+    rows = doc.xpath("//table[@summary='My Account - Activity log']/tbody/tr")
+    rows.collect do |row|
+      # get column text from rows
+      columns = row.xpath('td').collect do |column|
+        column.inner_text.strip
+      end
+
+      # ignore the header and other column types
+      if columns[4] == 'Hire'
+        start_time = parse_time(columns[0])
+        end_time = parse_time(columns[1])
+        cost = parse_cost(columns[5])
+        duration = ((end_time - start_time) / 60).to_i
+        {
+          :start_time => start_time,
+          :start_station => columns[2],
+          :end_time => end_time,
+          :end_station => columns[3],
+          :cost => cost,
+          :duration => duration
+        }
+      else
+        nil
+      end
+    end.compact
+  end
+
   class AuthenticateError < Exception
   end
 
@@ -55,7 +93,23 @@ class CycleHireApi
       cookie.split(';').first
     end.join('; ')
   end
+
+  # the data in the tables is seperated by a br, which the inner_text
+  # method strips, as such dates come in as "11 Oct 201109:15"
+  def parse_time(string)
+    if string =~ /(.*)([0-9]{2}:[0-9]{2})/
+      Time.parse($1 + " " + $2 + " BST")
+    end
+  end
+
+  def parse_cost(string)
+    if string =~ /£([0-9]+\.[0-9]{2})/
+      $1.to_f
+    end
+  end
 end
 
 api = CycleHireApi.new
 api.authenticate($username, $password)
+data = api.fetch_history
+puts data.to_yaml
