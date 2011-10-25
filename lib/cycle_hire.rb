211585c42cd8e54532dd80611ee9d93f55cb0f3f
Luca Spiller <luca@stackednotion.com>
2011-10-25 18:39:49 +0100

    Move api into seperate library

---
+++ lib/cycle_hire.rb
@@ -0,0 +1,5 @@
+module CycleHire
+  ROOT = File.expand_path(File.dirname(__FILE__))
+
+  autoload :API, "#{ROOT}/cycle_hire/api"
+end
---
+++ lib/cycle_hire/api.rb
@@ -0,0 +1,52 @@
+require 'httparty'
+require 'nokogiri'
+
+class CycleHire::API
+  include HTTParty
+
+  base_uri 'https://web.barclayscyclehire.tfl.gov.uk/'
+
+  def authenticate(username, password)
+    # initial request to get csrf
+    csrf_response = make_request(:get, '/')
+    doc = Nokogiri::HTML(csrf_response.body)
+    csrf = doc.xpath("//input[@id='login__csrf_token']").first.attribute('value').value
+
+    # authentication request
+    options = {
+      :query => {
+        'login[Email]' => username,
+        'login[Password]' => password,
+        'login[_csrf_token]' => csrf
+      },
+      :headers => {
+        'Content-Type' => 'application/x-www-form-urlencoded',
+      }
+    }
+    authentication_response = make_request(:post, '/', options)
+    raise AuthenticationError unless authentication_response.body =~ /Account Summary/
+    true
+  end
+
+  class AuthenticationError < Exception
+  end
+
+  protected
+
+  def make_request(method, path, options = {})
+    options[:headers] ||= {}
+    options[:headers]['Cookie'] = @cookies if @cookies
+
+    response = self.class.send(method, path, options)
+    @cookies = get_cookies_from_response(response)
+    response
+  end
+
+  def get_cookies_from_response(httparty_response)
+    # TODO there must be a better way to do this
+    cookies_array = httparty_response.response.to_hash['set-cookie'] || []
+    cookies_array.map do |cookie|
+      cookie.split(';').first
+    end.join('; ')
+  end
+end
