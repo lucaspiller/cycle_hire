3f127f5dfd59065fe9c0d5926e0407de4cb10f3f
Luca Spiller <luca@stackednotion.com>
2011-11-23 16:05:02 +0000

    Version bump (v1.0.2)

--- lib/cycle_hire.rb
+++ lib/cycle_hire.rb
@@ -1,6 +1,6 @@
 module CycleHire
   ROOT = File.expand_path(File.dirname(__FILE__))
-  VERSION = '1.0.1'
+  VERSION = '1.0.2'
 
   autoload :Session, "#{ROOT}/cycle_hire/session"
   autoload :JourneyParser, "#{ROOT}/cycle_hire/journey_parser"
