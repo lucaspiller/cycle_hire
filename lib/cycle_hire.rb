4e7b9538d30c3eb37aa938c66f9247754c0da3bc
Luca Spiller <luca@stackednotion.com>
2011-11-23 15:03:00 +0000

    Initial implementation of dock status (WARNING: major refactoring required)

--- lib/cycle_hire.rb
+++ lib/cycle_hire.rb
@@ -1,6 +1,15 @@
+require 'open-uri'
+
 module CycleHire
   ROOT = File.expand_path(File.dirname(__FILE__))
 
   autoload :Session, "#{ROOT}/cycle_hire/session"
   autoload :HistoryParser, "#{ROOT}/cycle_hire/history_parser"
+  autoload :StatusParser, "#{ROOT}/cycle_hire/status_parser"
+
+  def self.stations
+    body = open("https://web.barclayscyclehire.tfl.gov.uk/maps").read
+    parser = StatusParser.new
+    parser.parse(body)
+  end
 end
---
+++ lib/cycle_hire/status_parser.rb
@@ -0,0 +1,26 @@
+class CycleHire::StatusParser
+  STATION_REGEX = /\{id:"(\d+)".+?name:"(.+?)".+?lat:"(.+?)".+?long:"(.+?)".+?nbBikes:"(\d+)".+?nbEmptyDocks:"(\d+)".+?installed:"(.+?)".+?locked:"(.+?)".+?temporary:"(.+?)"\}/
+
+  def parse(body)
+    body.scan(STATION_REGEX).map do |station|
+      parse_station(station)
+    end
+  end
+
+  protected
+
+  def parse_station(st)
+    {
+      :id => st[0],
+      :name => (st[1].split(',')[0] || '').strip,
+      :area => (st[1].split(',')[1] || '').strip,
+      :lat => st[2].to_f,
+      :long => st[3].to_f,
+      :bikes => st[4].to_i,
+      :empty_docks => st[5].to_i,
+      :installed => (st[6] == 'true'),
+      :locked => (st[7] == 'true'),
+      :temporary => (st[8] == 'true')
+    }
+  end
+end
